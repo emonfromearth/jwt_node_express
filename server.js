33ba4886a34b5ac9a111d360d5059b7e7913710b
Sabbir Hosen Emon <qzjaman@gmail.com>
2022-01-23 23:52:01 +0600

    jwt creates on user creation

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
+require('dotenv').config(); // so that we can use .env file's variables
+
 const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken');
+
+// so that we can use json from the body
+app.use(express.json());
 
 const posts = [
     {
-        id: 1,
-        title: 'Post 1',
-        body: 'This is post 1'
+        username: 'Emon',
+        title: 'This is post 1'
     },
     {
-        id: 2,
-        title: 'Post 2',
-        body: 'This is post 2'
+        username: 'Kemon',
+        title: 'This is post 2'
     }
 ];
 
-app.get('/', (req, res) => {
+app.get('/posts', (req, res) => {
     res.json(posts)
+});
+
+app.post('/login', (req, res) => {
+    const username = req.body.username
+    const user = { name: username}
+
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    res.json({accessToken})
 });
 
 app.listen(3000)
