bd75ae5b743c95fb4d44a7debb29531f929cc6d4
Sabbir Hosen Emon <qzjaman@gmail.com>
2022-01-24 00:04:10 +0600

    filter posts using logged in users jwt

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ const posts = [
     }
 ];
 
-app.get('/posts', (req, res) => {
-    res.json(posts)
+app.get('/posts', authenticateToken, (req, res) => {
+    res.json(posts.filter(posts => posts.username === req.user.name))
 });
 
 app.post('/login', (req, res) => {
@@ -30,4 +30,16 @@ app.post('/login', (req, res) => {
     res.json({accessToken})
 });
 
+function authenticateToken(req, res, next){
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if(token == null) return res.sendStatus(401)
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+
+}
 app.listen(3000)
